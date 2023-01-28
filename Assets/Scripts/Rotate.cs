d31a0b6a9fc341e9b645d333112fdf99fbfe7deb
schmidtc1 <62030095+schmidtc1@users.noreply.github.com>
2023-01-28 02:50:31 -0500

    Add functionality for fire barrier rotation
    Created a fire barrier that rotates with mouse around Goo
        - Uses new sprites that were added in
            - Meteor sprite will also come from them
        - Need to implement meteor collision on barrier/goo/wall functionality for it to either:
            - bounce off of barrier or wall
            - kill Goo/hurt Goo

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Transform orb;
+    public float radius;
+ 
+    private Transform pivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pivot = orb.transform;
+        transform.parent = pivot;
+        transform.position += Vector3.up * radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKey(KeyCode.Q)) {
+        //     curAngle += 0.7f;
+        // }
+        // else if (Input.GetKey(KeyCode.E)) {
+        //     curAngle -= 0.7f;
+        // }
+
+        Vector3 orbVector = Camera.main.WorldToScreenPoint(orb.position);
+        orbVector = Input.mousePosition - orbVector;
+        float angle = Mathf.Atan2(orbVector.y, orbVector.x) * Mathf.Rad2Deg;
+ 
+        pivot.position = orb.position;
+        pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+    }
+}
